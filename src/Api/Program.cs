24a1740055ff8e351797e78abdbf2b798dc4c118
pachaitech2017 <pachaitech2017@gmail.com>
2025-09-29 06:40:25 +0530

    Latest

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lotus API 2025 v1");
+    c.RoutePrefix = string.Empty;
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
