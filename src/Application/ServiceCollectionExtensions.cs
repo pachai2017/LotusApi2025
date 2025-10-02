c6f6dba0a74060c2891082992ee13f3f131279e4
pachai2017 <51010455+pachai2017@users.noreply.github.com>
2025-10-02 18:05:52 +0530

    Remove redundant manual MediatR handler registrations

--- src/Application/ServiceCollectionExtensions.cs
+++ src/Application/ServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using LotusFive.Application.Common.Commands;
-using LotusFive.Application.Common.Queries;
 using MediatR;
 using MediatR.Registration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,11 +14,6 @@ namespace LotusFive.Application
                 cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
             });
 
-            services.AddTransient(typeof(IRequestHandler<,>), typeof(GetAllEntitiesQueryHandler<>));
-            services.AddTransient(typeof(IRequestHandler<,>), typeof(GetEntityByIdQueryHandler<,>));
-            services.AddTransient(typeof(IRequestHandler<,>), typeof(CreateEntityCommandHandler<>));
-            services.AddTransient(typeof(IRequestHandler<,>), typeof(UpdateEntityCommandHandler<,>));
-            services.AddTransient(typeof(IRequestHandler<,>), typeof(DeleteEntityCommandHandler<,>));
             return services;
         }
     }
